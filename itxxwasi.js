0426648baec10ea0900ed11d50d9711a7f4db65f
FELIXTZ <hanstzgamestech@gmail.com>
2025-01-12 18:50:12 +0300

    Update itxxwasi.js

--- itxxwasi.js
+++ itxxwasi.js
@@ -1,30 +1,12 @@
 const express = require('express');
+const router = require('./router');
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let server = require('./wasiqr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/wasiqr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/wasipage.html')
-})
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/', router);
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star
-
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
-/**
-    powered by wasi tech team 
-    join Whatsapp channel for more updates 
-    **/
+  console.log(`Server running on http://localhost:${PORT}`);
+});
